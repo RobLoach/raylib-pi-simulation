558bc93f9aad03a2b921af236e0e43a58bf6a6c1
Rob Loach <robloach@gmail.com>
2025-03-16 01:31:28 -0400

    Update

--- raylib-pi-simulation.c
+++ raylib-pi-simulation.c
@@ -1,23 +1,5 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window (adapted for HTML5 platform)
-*
-*   This example is prepared to compile for PLATFORM_WEB and PLATFORM_DESKTOP
-*   As you will notice, code structure is slightly different to the other examples...
-*   To compile it for PLATFORM_WEB just uncomment #define PLATFORM_WEB at beginning
-*
-*   This example has been created using raylib 1.3 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2015 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 
-//#define PHYSAC_NO_THREADS
-//#define PHYSAC_MALLOC MemAlloc
-//#define PHYSAC_FREE MemFree
 #define PHYSAC_IMPLEMENTATION
 #include "vendor/Physac/src/physac.h"
 
@@ -36,8 +18,12 @@ typedef struct GameState {
 
     Rectangle bigRectangleStart;
     Rectangle smallRectangleStart;
+    PhysicsBody wall;
 
     Color background;
+    Color textColor;
+
+    int collisions;
 } GameState;
 
 void UpdateDrawFrame(void* game);     // Update and Draw one frame
@@ -49,12 +35,32 @@ void reset(GameState* game, bool start) {
     }
 
     float size = 100.0f;
-    Rectangle bigRectangleStart = (Rectangle){SCREEN_WIDTH / 2 + SCREEN_WIDTH / 4, SCREEN_WIDTH / 2 - size * 0.5f, 200, 200};
-    Rectangle smallRectangleStart = (Rectangle){SCREEN_WIDTH / 4, bigRectangleStart.y + bigRectangleStart.height - 150, 100, 100};
-    game->bigRectangle = CreatePhysicsBodyRectangle((Vector2){bigRectangleStart.x, bigRectangleStart.y}, bigRectangleStart.width, bigRectangleStart.height, 1.0f);
-    game->smallRectangle = CreatePhysicsBodyRectangle((Vector2){smallRectangleStart.x, smallRectangleStart.y}, smallRectangleStart.width, smallRectangleStart.height, 10.0f);
+    game->collisions = 0;
+    game->bigRectangleStart = (Rectangle){SCREEN_WIDTH / 2 + SCREEN_WIDTH / 4, SCREEN_WIDTH / 2 - size * 0.5f, 200, 200};
+    game->smallRectangleStart = (Rectangle){SCREEN_WIDTH / 4, game->bigRectangleStart.y + game->bigRectangleStart.height - 150, 100, 100};
 
-    PhysicsAddForce(game->bigRectangle, (Vector2){-5000.0f, 0});
+    game->bigRectangle = CreatePhysicsBodyRectangle((Vector2){game->bigRectangleStart.x, game->smallRectangleStart.y}, game->smallRectangleStart.width, game->smallRectangleStart.height, 1.0f);
+    game->bigRectangle->freezeOrient = true;
+    game->bigRectangle->isGrounded = true;
+    game->bigRectangle->mass = 1.0f;
+    game->bigRectangle->staticFriction = 0.0f;
+    game->bigRectangle->useGravity = false;
+    //game->bigRectangle->velocity = (Vector2) {-0.5f, 0.0f};
+    game->bigRectangle->force.y = 0.0f;
+    game->bigRectangle->force.x = -1000.0f;
+    
+    game->smallRectangle = CreatePhysicsBodyRectangle((Vector2){game->smallRectangleStart.x, game->smallRectangleStart.y}, game->smallRectangleStart.width, game->smallRectangleStart.height, 1.0f);
+    game->smallRectangle->freezeOrient = true;
+    game->smallRectangle->isGrounded = true;
+    game->smallRectangle->mass = 10000.0f;
+    game->smallRectangle->staticFriction = 0.0f;
+    game->smallRectangle->useGravity = false;
+
+    game->wall = CreatePhysicsBodyRectangle((Vector2) { -1.0f, 0.0f}, 1.0f, SCREEN_HEIGHT * 2.0f, 9999999.0f);
+    game->wall->freezeOrient = true;
+    game->wall->isGrounded = true;
+    game->wall->useGravity = true;
+    game->wall->mass = 999999999999.0f;
 }
 
 int main() {
@@ -66,6 +72,7 @@ int main() {
     game.background = (Color){40, 42, 54, 255};
     game.bigRectangleColor = (Color){139, 233, 253, 255};
     game.smallRectangleColor = (Color){98, 114, 164, 255};
+    game.textColor = (Color){248, 248, 242, 255};
 
     reset(&game, true);
 
@@ -81,6 +88,7 @@ int main() {
 
     DestroyPhysicsBody(game.bigRectangle);
     DestroyPhysicsBody(game.smallRectangle);
+    DestroyPhysicsBody(game.wall);
     ClosePhysics();
     CloseWindow();
 
@@ -92,6 +100,32 @@ void UpdateDrawFrame(void* userdata) {
 
     if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
         //reset(game, false);
+    }
+
+
+
+
+    // Draw created physics bodies
+    int bodiesCount = GetPhysicsBodiesCount();
+    for (int i = 0; i < bodiesCount; i++) {
+        PhysicsBody body = GetPhysicsBody(i);
+        // if (body->position.x < game->smallRectangleStart.width / 2.0f && body->velocity.x <= 0.0f) {
+        //     body->velocity.x *= -1.0f;
+        // }
+
+
+
+        // body->freezeOrient = true;
+        // body->isGrounded=true;
+
+
+        int vertexCount = GetPhysicsShapeVerticesCount(i);
+        for (int j = 0; j < vertexCount; j++)  {
+            Vector2 vertexA = GetPhysicsShapeVertex(body, j);
+            int jj = (((j + 1) < vertexCount) ? (j + 1) : 0);   // Get next vertex or first to close the shape
+            Vector2 vertexB = GetPhysicsShapeVertex(body, jj);
+            DrawLineEx(vertexA, vertexB, 3.0f, i % 2 == 0 ? game->bigRectangleColor : game->smallRectangleColor);     // Draw a line between two vertex positions
+        }
     }
 
     //SetPhysicsTimeStep(GetFrameTime());
@@ -113,6 +147,8 @@ void UpdateDrawFrame(void* userdata) {
                 DrawLineEx(vertexA, vertexB, 3.0f, i % 2 == 0 ? game->bigRectangleColor : game->smallRectangleColor);     // Draw a line between two vertex positions
             }
         }
+
+        DrawText(TextFormat("Collisions: %d", game->collisions), 10, 10, 24, game->textColor);
     }
     EndDrawing();
 }
